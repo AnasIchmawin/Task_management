3d0efe8d3f2fd8fb1d3ef0b7b302e6c6d2aad45e
FatihMedAmine <153363599+FatihMedAmine@users.noreply.github.com>
2024-04-02 22:34:24 +0000

    L'interface du listes

---
+++ src/presentation/listes/ListFormController.java
@@ -0,0 +1,53 @@
+package presentation.listes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ListFormController {
+
+    @FXML
+    private Button Ajouter;
+
+    @FXML
+    private Button Archive;
+
+    @FXML
+    private Button List;
+
+    @FXML
+    private Button Ordonner;
+
+    @FXML
+    private Button Projets;
+
+    @FXML
+    private TextField SearchInput;
+
+    @FXML
+    void AjouterButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ArchiveButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ListButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OrdonnerButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ProjetsButton(ActionEvent event) {
+
+    }
+
+}
