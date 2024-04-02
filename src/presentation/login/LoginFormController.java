0932c5ef35da61052615635f2da84f1014935687
FatihMedAmine <153363599+FatihMedAmine@users.noreply.github.com>
2024-04-02 02:16:40 +0000

    Add files via upload

---
+++ src/presentation/login/LoginFormController.java
@@ -0,0 +1,21 @@
+package presentation.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    @FXML
+    private Button ConnexionButton;
+
+    @FXML
+    private TextField InputEmail;
+
+    @FXML
+    void ConnexionButton(ActionEvent event) {
+
+    }
+
+}
