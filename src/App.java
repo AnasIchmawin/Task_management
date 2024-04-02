c8b9778cf46cae2d6f1175f5679ea44c2433ee33
FatihMedAmine <153363599+FatihMedAmine@users.noreply.github.com>
2024-04-02 14:02:08 +0000

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/presentation/login/LoginForm.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1300, 652);
+            stage.setScene(scene);
+            stage.setTitle("TO DO LIST");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
