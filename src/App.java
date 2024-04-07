3a76025bc3914221f1a56de3acbeea329cc03995
aichmawi <ichmawinanas7@gmail.com>
2024-04-07 23:14:57 +0000

    #

--- src/App.java
+++ src/App.java
@@ -1,12 +1,14 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import presentation.listes.screenList;
 import presentation.login.screenLogin;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        screenLogin login = new screenLogin(); 
+        //screenList list = new screenList(); 
+        screenLogin login = new screenLogin();
         login.start(primaryStage); // Appelez la méthode start() de Login
     }
 
