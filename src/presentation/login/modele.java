55a36759d71d24bccf582a1f55a494821130b082
FatihMedAmine <153363599+FatihMedAmine@users.noreply.github.com>
2024-04-08 16:57:56 +0000

    Update modele.java
    j'ai ajoute les donnees de l'interface Login et les getters et setters

--- src/presentation/login/modele.java
+++ src/presentation/login/modele.java
@@ -1,5 +1,20 @@
 package presentation.login;
 
 public class modele {
+    private String Gmail ;
 
+    public modele(String Gmail) {
+        this.Gmail = Gmail;
+    }
+
+    public String getGmail() {
+        return Gmail;
+    }
+
+    public void setGmail(String Gmail) {
+        this.Gmail = Gmail;
+    }
 }
+
+
+
