71ea244e8f4a1ceee60af6e8af5bef826dd533b6
FatihMedAmine <153363599+FatihMedAmine@users.noreply.github.com>
2024-04-09 01:13:27 +0000

    Update screenList.java

--- src/presentation/listes/screenList.java
+++ src/presentation/listes/screenList.java
@@ -5,18 +5,17 @@ import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
-
-
 
 public class screenList extends Application {
 
@@ -27,8 +26,8 @@ public class screenList extends Application {
         AnchorPane root = new AnchorPane();
         root.setStyle("-fx-background:#ffffff"); // la color de background
 
-        // Barre de navigation
 
+        // Barre de navigation
         HBox navigationBar = new HBox();
         navigationBar.getStyleClass().add("navigationBar"); // Appliquer le style depuis le fichier CSS
 
@@ -48,22 +47,22 @@ public class screenList extends Application {
         TextField searchField = new TextField();
         searchField.setPromptText("Rechercher");
 
-
         // Appliquer le style de bouton depuis le fichier CSS
         LeftButton.getStyleClass().add("button-left-style");
-        ListesButton.getStyleClass().add("button-clicked-style");
+        ListesButton.getStyleClass().add("button-style");
         ProjetsButton.getStyleClass().add("button-style");
         ArchiveButton.getStyleClass().add("button-style");
         OrdonnerButton.getStyleClass().add("button-style");
         AjouterButton.getStyleClass().add("button-style");
         searchField.getStyleClass().add("search-field-style");
 
-        // Appliquer une marge à gauche aux boutons
+        // Appliquer une marge à gauche aux boutons sur le navbar 
         HBox.setMargin(ListesButton, new Insets(0, 0, 0, 40));
         HBox.setMargin(ProjetsButton, new Insets(0, 0, 0, 15));
         HBox.setMargin(ArchiveButton, new Insets(0, 0, 0, 15));
         HBox.setMargin(LeftButton, new Insets(0, 0, 0, 15));
-
+        
+        // Logo de la flèche gauche
         try {
             Image leftIcon = new Image("file:./Pictures/left-arrow.png");
             ImageView leftIconView = new ImageView(leftIcon);
@@ -73,7 +72,8 @@ public class screenList extends Application {
         } catch (Exception e) {
             System.out.println("Error loading the left arrow icon");
         }
-
+        
+        // Logo de la boton ordonner
         try {
             Image OrdonnerIcon = new Image("file:./Pictures/folder.png");
             ImageView OrdonnerIconView = new ImageView(OrdonnerIcon);
@@ -83,6 +83,8 @@ public class screenList extends Application {
         } catch (Exception e) {
             System.out.println("Error loading the Ordonner icon");
         }
+
+        // Logo de la boton ajouter
         try {
             Image AjouterIcon = new Image("file:./Pictures/add.png");
             ImageView AjouterIconView = new ImageView(AjouterIcon);
@@ -93,43 +95,45 @@ public class screenList extends Application {
             System.out.println("Error loading the Ajouter icon");
         }
 
+        // Logo de la barre de recherche
         Image searchIcon = new Image("file:./Pictures/loupe.png");
         ImageView searchIconView = new ImageView(searchIcon);
         searchIconView.setFitWidth(20);
         searchIconView.setFitHeight(20);
 
-        // rectangle de background gray
+        // background gray
         Rectangle BkGround = new Rectangle();
         BkGround.setWidth(screenWidth - 40); // Définir la largeur du rectangle
         BkGround.setHeight(590); // Définir la hauteur du rectangle
-        BkGround.setStroke(Color.web("#112D4E")); // Définir la couleur de la bordure
-        BkGround.setStrokeWidth(2.0); // Définir la largeur de la bordure
+        BkGround.setStroke(Color.BLACK); // Définir la couleur de la bordure
         BkGround.setFill(Color.rgb(240, 240, 240)); // Remplissage avec la couleur #F0F0F0
-        BkGround.setArcWidth(15); // Définir le rayon horizontal des coins
-        BkGround.setArcHeight(15); // Définir le rayon vertical des coins
+
+        // Le contenir des listes
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(10); // Appliquer un espacement vertical entre les éléments du GridPane
+        gridPane.setHgap(10); // Appliquer un espacement horizontal entre les éléments du GridPane
+        gridPane.setPrefWidth(screenWidth - 100);
+        gridPane.setPrefHeight(450);
+        gridPane.setStyle("-fx-background-color: #F0F0F0;");
+
+        ScrollPane scrollPane = new ScrollPane(gridPane);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setStyle("-fx-background-color: transparent; -fx-background: transparent;");
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER); // Désactiver la barre de défilement horizontale
+
+        // Action du bouton Ajouter
+        AjouterButton.setOnAction(event -> {
+            ListFormController.handleAjouterButtonAction(gridPane);
+        });
+        
+        
 
         // L'ajout des boutons à la barre de navigation
         navigationBar.getChildren().addAll(LeftButton, ListesButton, ProjetsButton, ArchiveButton);
 
 
- // probleme du style externe donc je vais le mettre ici
-        Button Liste1 = new Button("Liste 1");
-        Liste1.setStyle("-fx-background-color: #112D4E; " +
-                "-fx-background-radius: 10px; " +
-                "-fx-pref-width: 170px; " +
-                "-fx-pref-height: 60px;"+
-                "-fx-text-fill: #ffffff;"+
-                "-fx-font-size: 15px;");
-
-        try {
-            Image Liste1Icon = new Image("file:./Pictures/to-do.png");
-            ImageView Liste1IconView = new ImageView(Liste1Icon);
-            Liste1IconView.setFitWidth(20);
-            Liste1IconView.setFitHeight(20);
-            Liste1.setGraphic(Liste1IconView);
-        } catch (Exception e) {
-            System.out.println("Error loading the Liste1 icon");
-        }
+        
         // navbar position
         AnchorPane.setTopAnchor(navigationBar, 0.0);
         AnchorPane.setLeftAnchor(navigationBar, 20.0);
@@ -154,20 +158,25 @@ public class screenList extends Application {
         AnchorPane.setTopAnchor(searchIconView, 80.0); // Spécifiez la distance depuis le haut
         AnchorPane.setLeftAnchor(searchIconView, screenWidth - 127);
 
-        // la positions du liste 1
-        AnchorPane.setTopAnchor(Liste1, 150.0); // Spécifiez la distance depuis le haut
-        AnchorPane.setLeftAnchor(Liste1, 85.0); // Spécifiez la distance depuis la gauche
+        // la positions du scrollPane
+        AnchorPane.setTopAnchor(scrollPane, 140.0); // Spécifiez la distance depuis le haut
+        AnchorPane.setLeftAnchor(scrollPane, 75.0); // Spécifiez la distance depuis la gauche
 
+       // la positions du gridPane
+        AnchorPane.setTopAnchor(gridPane, 140.0); // Spécifiez la distance depuis le haut
+        AnchorPane.setLeftAnchor(gridPane, 75.0); // Spécifiez la distance depuis la gauche
 
         // Ajout de la barre de navigation à la racine
+        
         root.getChildren().add(BkGround);
+     //   root.getChildren().add(gridPane);
+        root.getChildren().add(scrollPane);
         AnchorPane.setTopAnchor(navigationBar, 0.0);
         root.getChildren().addAll(navigationBar);
         root.getChildren().add(OrdonnerButton);
         root.getChildren().add(AjouterButton);
         root.getChildren().add(searchField);
         root.getChildren().add(searchIconView);
-        root.getChildren().add(Liste1);
 
         Scene scene = new Scene(root, 600, 400);
         scene.getStylesheets().add(getClass().getResource("ListStyle.css").toExternalForm()); // Charger le fichier CSS
