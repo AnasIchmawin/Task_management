9218cbf06c149bf0b01161a4e48654e99ab3d6b7
FatihMedAmine <153363599+FatihMedAmine@users.noreply.github.com>
2024-04-10 12:06:48 +0000

    l'ajout du controleur de Seance

---
+++ src/presentation/seance/SceanceFormController.java
@@ -0,0 +1,11 @@
+package presentation.seance;
+import javafx.scene.control.TextArea;
+
+public class SceanceFormController {
+
+    public static void handleEditerButton(TextArea ZoneDescription){
+            ZoneDescription.setEditable(true);
+
+        };
+    
+}
