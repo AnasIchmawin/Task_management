60f759ee7451bfd0440376222dc16679033c02c0
FatihMedAmine <153363599+FatihMedAmine@users.noreply.github.com>
2024-04-11 01:40:50 +0000

    Update SceanceFormController.java

--- src/presentation/seance/SceanceFormController.java
+++ src/presentation/seance/SceanceFormController.java
@@ -1,11 +1,17 @@
 package presentation.seance;
 import javafx.scene.control.TextArea;
 
+
 public class SceanceFormController {
 
     public static void handleEditerButton(TextArea ZoneDescription){
             ZoneDescription.setEditable(true);
 
+        };
+
+    public static void handleSaveButton(TextArea ZoneDescription , TextArea ZoneNote){
+            ZoneDescription.setEditable(false);
+            ZoneNote.setEditable(false);
         };
     
 }
