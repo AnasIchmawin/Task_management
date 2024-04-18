b5ed69bf8d081aee7e491741604fe56bf9b50b6a
FatihMedAmine <153363599+FatihMedAmine@users.noreply.github.com>
2024-04-18 19:22:55 +0100

    Update LoginFormController.java

--- src/presentation/login/LoginFormController.java
+++ src/presentation/login/LoginFormController.java
@@ -1,21 +1,13 @@
 package presentation.login;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 
-public class LoginFormController {
 
-    @FXML
-    private Button ConnexionButton;
+public class LoginController {
 
-    @FXML
-    private TextField InputEmail;
+    
 
-    @FXML
-    void ConnexionButton(ActionEvent event) {
-
+    public void handleLoginButtonClick() {
+        System.out.println("Login button clicked");
     }
 
 }
