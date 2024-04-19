60f6d5b1157df6589126bc66d901a35d6a54fbff
aichmawi <ichmawinanas7@gmail.com>
2024-04-19 22:52:28 +0100

    .

---
+++ src/presentation/login/LoginController.java
@@ -0,0 +1,13 @@
+package presentation.login;
+
+
+
+public class LoginController {
+
+    
+
+    public void handleLoginButtonClick() {
+        System.out.println("Login button clicked");
+    }
+
+}
