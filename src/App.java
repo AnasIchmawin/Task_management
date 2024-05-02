34521605f375e4ff3185707761fd61d585df9dc0
aichmawi <aichmawi@student.1337.ma>
2024-05-02 13:23:26 +0100

    #

--- src/App.java
+++ src/App.java
@@ -1,47 +1,23 @@
 
-import java.util.List;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-import persistence.DBConnection;
-import presentation.archive.ArchiveFormController;
-import presentation.archive.ArchiveFormView;
-import presentation.listes.ListeFormController;
-import presentation.listes.ListeFormView;
-import presentation.login.LoginController;
-import presentation.login.LoginFormView;
-import presentation.projets.ProjetsFormController;
-import presentation.projets.ProjetsFormView;
-import presentation.seance.SceanceFormController;
-import presentation.seance.SeanceFormView;
+import presentation.tache_ajoute.ViewFromTacheAjout;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // ListeFormController controller = new ListeFormController();
-        // ListeFormView Seance = new ListeFormView(controller);
-        // Seance.start(primaryStage);
-
-        ProjetsFormController controller = new ProjetsFormController();
-        ProjetsFormView projets = new ProjetsFormView(controller);
-        projets.start(primaryStage);
-
-        // ArchiveFormController controller = new ArchiveFormController();
-        // ArchiveFormView archive = new ArchiveFormView(controller);
-        // archive.start(primaryStage);
+    public void start(Stage primaryStage) {
+    	ViewFromTacheAjout view = new ViewFromTacheAjout();
+        Scene scene = new Scene(view.getView(), 1160, 652);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        try {
-            // Connexion à la base de données
-            DBConnection dbConnection = DBConnection.getInstance();
-            System.out.println("Connexion à la base de données réussie");
-
-        } catch (Exception e) {
-            System.out.println("Erreur de connexion à la base de données");
-        }
-        // Lancer l'application
         launch(args);
     }
 }
