a2efd199c236c48fede494acda9ea647b085bb7b
souhailmossalime <167001672+souhailmossalime@users.noreply.github.com>
2024-05-02 04:25:05 +0100

    Update DocumentFormcontroleur.java

--- src/presentation/document_ajoute/DocumentFormcontroleur.java
+++ src/presentation/document_ajoute/DocumentFormcontroleur.java
@@ -1,21 +1,6 @@
 package presentation.document_ajoute;
 
-import javafx.scene.control.TextArea;
-
 public class DocumentFormcontroleur {
-	
-	
-	public static void handleEditerButton(TextArea ZoneDescription){
-        ZoneDescription.setEditable(true);
-
-    };
-    
-	public static void handleSaveButton(TextArea ZoneDescription){
-        ZoneDescription.setEditable(false);
- 
-        
-     
-    };
-
-
+	 {
+    // You can add event handling logic here if needed
 }
