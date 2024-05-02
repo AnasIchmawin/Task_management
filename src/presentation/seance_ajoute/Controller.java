5fcd39f6f5523e20456c6d4189a00261066e5dd5
souhailmossalime <167001672+souhailmossalime@users.noreply.github.com>
2024-05-02 04:30:31 +0100

    Update and rename controleur.java to Controller.java

---
+++ src/presentation/seance_ajoute/Controller.java
@@ -0,0 +1,6 @@
+package presentation.seance_ajoute;
+
+
+public class Controller {
+    // You can add event handling logic here if needed
+}
--- src/presentation/seance_ajoute/controleur.java
+++
@@ -1,5 +0,0 @@
-package presentation.seance_ajoute;
-
-public class controleur {
-
-}
