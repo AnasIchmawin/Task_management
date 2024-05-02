38255290556cdb31ee0c7ddb8c67c208a52aec6b
souhailmossalime <167001672+souhailmossalime@users.noreply.github.com>
2024-05-02 04:21:54 +0100

    Update and rename controleur.java to ControllerFromTacheAjout.java

---
+++ src/presentation/tache_ajoute/ControllerFromTacheAjout.java
@@ -0,0 +1,5 @@
+package application;
+
+public class ControllerFromTacheAjout {
+    // You can add event handling logic here if needed
+}
--- src/presentation/tache_ajoute/controleur.java
+++
@@ -1,5 +0,0 @@
-package presentation.tache_ajoute;
-
-public class controleur {
-
-}
