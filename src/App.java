b07b2636ed5d0477cf5703ad2f8ebd2a7bdb6686
aichmawi <aichmawi@student.1337.ma>
2024-05-02 23:19:26 +0100

    #

--- src/App.java
+++ src/App.java
@@ -1,23 +1,47 @@
 
+import java.util.List;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import presentation.tache_ajoute.ViewFromTacheAjout;
+import persistence.DBConnection;
+import presentation.archive.ArchiveFormController;
+import presentation.archive.ArchiveFormView;
+import presentation.listes.ListeFormController;
+import presentation.listes.ListeFormView;
+import presentation.login.LoginController;
+import presentation.login.LoginFormView;
+import presentation.projets.ProjetsFormController;
+import presentation.projets.ProjetsFormView;
+import presentation.seance.SceanceFormController;
+import presentation.seance.SeanceFormView;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-    	ViewFromTacheAjout view = new ViewFromTacheAjout();
-        Scene scene = new Scene(view.getView(), 1160, 652);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("JavaFX App");
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        // ListeFormController controller = new ListeFormController();
+        // ListeFormView Seance = new ListeFormView(controller);
+        // Seance.start(primaryStage);
+
+        SceanceFormController controller = new SceanceFormController();
+        SeanceFormView projets = new SeanceFormView(controller);
+        projets.start(primaryStage);
+
+        // ArchiveFormController controller = new ArchiveFormController();
+        // ArchiveFormView archive = new ArchiveFormView(controller);
+        // archive.start(primaryStage);
     }
 
     public static void main(String[] args) {
+        try {
+            // Connexion à la base de données
+            DBConnection dbConnection = DBConnection.getInstance();
+            System.out.println("Connexion à la base de données réussie");
+
+        } catch (Exception e) {
+            System.out.println("Erreur de connexion à la base de données");
+        }
+        // Lancer l'application
         launch(args);
     }
 }
