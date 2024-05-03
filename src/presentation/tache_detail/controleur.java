bbdfb1f1ad3fd2b54dae3a26d28d2df271e7b38b
IMANI Mourad <imanimourad.92@gmail.com>
2024-05-03 02:48:08 +0100

    commit tache_detail folder

--- src/presentation/tache_detail/controleur.java
+++ src/presentation/tache_detail/controleur.java
@@ -1,5 +1,42 @@
 package presentation.tache_detail;
 
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
 public class controleur {
+
+    public void handleAjouterButtonAction(HBox gridPane) {
+    }
+    public void handleAjouterButtonAction(GridPane gridPane) {
+        
+        Button newListButton = new Button("Liste");
+        newListButton.setStyle("-fx-background-color: #112D4E; " +
+                "-fx-background-radius: 10px; " +
+                "-fx-min-width: 70px; " +
+                "-fx-min-height: 40px;" +
+                "-fx-text-fill: #ffffff;" +
+                "-fx-font-size: 10px;");
+                int colIndex = gridPane.getChildren().size() % 3; // Calculating column index
+                int rowIndex = gridPane.getChildren().size() / 3; // Calculating row index
+
+                // Load list icon
+                try {
+                    Image listIcon = new Image("file:./Pictures/to-do.png");
+                    ImageView listIconView = new ImageView(listIcon);
+                    listIconView.setFitWidth(15);
+                    listIconView.setFitHeight(15);
+                    newListButton.setGraphic(listIconView);
+                    gridPane.add(newListButton, colIndex, rowIndex);
+
+                } catch (Exception e) {
+                    System.out.println("Erreur de chargement de l'icône de la liste : " + e.getMessage());
+                }
+            }
+    public void handleSaveButton(GridPane gridPane) {
+        // Save button;
+    }
 
 }
